b1b2d28d3139346d99a06e40c31f76442527074c
Lén <corbeau.lenaic@gmail.com>
2026-09-18 21:59:52 +0200

    Update function_pickup.gd

--- addons/godot-xr-tools/functions/function_pickup.gd
+++ addons/godot-xr-tools/functions/function_pickup.gd
@@ -408,8 +408,7 @@ func _on_grab_exited(target: Node3D) -> void:
 # When an object enters the ranged-grab cylinder
 func _on_ranged_entered(target: Node3D) -> void:
 	# reject objects which don't support picking up rangedly
-	var pickable := target as XRToolsPickable
-	if not pickable or not pickable.can_ranged_grab or not pickable.has_method('can_ranged_grab'):
+	if not "can_ranged_grab" in target or target.can_ranged_grab:
 		return
 
 	# ignore objects already known
